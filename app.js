5e9f03995076da45b8bb896b34055180df287b5c
GilbertRoss <bhuiyannazrul74@gmail.com>
2019-07-08 14:46:18 +0200

    update app.js - adding grid

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function createGrid(){
 
     for(let i = 0; i<16; i++){
         const div = document.createElement("div");
-        div.classList("grid");
+        div.classList.add("grid");
         container.appendChild(div);
 
     }
