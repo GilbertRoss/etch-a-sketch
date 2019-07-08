9338e53b45fa4f616e3d046152fb1f1ecfc3781b
GilbertRoss <bhuiyannazrul74@gmail.com>
2019-07-08 15:00:54 +0200

    adding number to each grid

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function createGrid(){
     for(let i = 0; i<16; i++){
         const div = document.createElement("div");
         div.classList.add("grid");
+        div.textContent(i);
         container.appendChild(div);
     }
 
