5ab69e9a0b551fb92f0af3c22305e1d0b8d2ce9b
GilbertRoss <bhuiyannazrul74@gmail.com>
2019-07-08 20:23:27 +0200

    editing clear button

--- app.js
+++ app.js
@@ -14,27 +14,19 @@ function createGrid(){
 
 }
 
-function deleteGrid(){
-
-    for(let i = 0; i<256; i++){
-        const div = document.createElement("div");
-        div.classList.add("grid");
-        div.textContent = i;
-        container.appendChild(div);
-    }
-
-}
 
 createGrid();
 
 var btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
-    createGrid();
+    var gridBehaviour = document.querySelector('.filledGrid');
+
+    gridBehaviour.setAttribute("class", "grid");
 });
 
-while(true){
+
 var gridBehaviour = document.querySelector('.grid');
 gridBehaviour.addEventListener('mouseover', () => {
     gridBehaviour.setAttribute("class", "filledGrid");
 });
-}
+
