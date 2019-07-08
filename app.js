0a58458b52dce8ee64024bba0e49f131174ba9c1
GilbertRoss <bhuiyannazrul74@gmail.com>
2019-07-08 14:47:52 +0200

    update

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ function createGrid(){
     for(let i = 0; i<16; i++){
         const div = document.createElement("div");
         div.classList.add("grid");
-        container.appendChild(div);
-
+        container.appendChild(content);
     }
 
 }
