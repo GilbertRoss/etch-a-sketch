332853a991a58f9c307e1cfcc6b31f8e0bd74211
GilbertRoss <bhuiyannazrul74@gmail.com>
2019-07-08 14:36:09 +0200

    createGrid function on app.js

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const container = document.getElementsByClassName(container);
-container.setAttribute("style", "background-color: red;");
+const container = document.getElementsByClassName(".container");
+
+
+
+
+function createGrid(){
+
+    for(let i = 0; i<16; i++){
+        const div = document.createElement("div");
+        div.classList("grid");
+        container.appendChild(div);
+
+    }
+
+}
+
+createGrid();
