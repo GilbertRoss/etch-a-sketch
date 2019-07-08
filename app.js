2d3d86667126e5784808f991295ae1fa51814b4c
GilbertRoss <bhuiyannazrul74@gmail.com>
2019-07-08 15:03:02 +0200

    adding textcontent to div

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function createGrid(){
     for(let i = 0; i<16; i++){
         const div = document.createElement("div");
         div.classList.add("grid");
-        div.textContent(i);
+        div.textContent = i;
         container.appendChild(div);
     }
 
